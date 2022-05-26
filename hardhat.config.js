ecd9886a7614faad657435ea104a01f176d24229
andre2130 <atrobinson30@gmail.com>
2022-05-26 00:00:02 -0400

    frontend styling

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,43 +13,43 @@ module.exports = {
     artifacts: './src/artifacts',
   },
   networks: {
-     
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/018a1557b0e34090a84c111da5c01426",
-      accounts: ["606ebc0fdf53ba7d03ffaf735064105f9e083119ab91f36e06c9fc67080f8323"],
-      gas: 2100000,
-      gasPrice: 8000000000,
-    },
-    mainnet: {
-      url: "https://mainnet.infura.io/v3/9e4c4b48907f4beba4ca0f3cc9d50ea2",
-      accounts: ["606ebc0fdf53ba7d03ffaf735064105f9e083119ab91f36e06c9fc67080f8323"],
-      gas: 2100000,
-      gasPrice: 30000000000,
-    },
-    ropsten: {
-      url: "https://ropsten.infura.io/v3/nada",
-      accounts: ["606ebc0fdf53ba7d03ffaf735064105f9e083119ab91f36e06c9fc67080f8323"],
-      gas: 2100000,
-      gasPrice: 8000000000,
-    },
-    
-    
-       hardhat: {
-      chainId: 1337
-    },
-    mainnet: {
-      url: "https://mainnet.infura.io/v3/9e4c4b48907f4beba4ca0f3cc9d50ea2",
-      accounts: ["nada"]
-    },
-    ropsten: {
-      url: "https://ropsten.infura.io/v3/9e4c4b48907f4beba4ca0f3cc9d50ea2",
-      accounts: ["nada"]
-    },
-    rinkeby: {
-      url: "https://rinkeby.infura.io/v3/9e4c4b48907f4beba4ca0f3cc9d50ea2",
-      accounts: ["nada"]
-    }
-   
+
+    // rinkeby: {
+    //   url: "https://rinkeby.infura.io/v3/018a1557b0e34090a84c111da5c01426",
+    //   accounts: ["606ebc0fdf53ba7d03ffaf735064105f9e083119ab91f36e06c9fc67080f8323"],
+    //   gas: 2100000,
+    //   gasPrice: 8000000000,
+    // },
+    // mainnet: {
+    //   url: "https://mainnet.infura.io/v3/9e4c4b48907f4beba4ca0f3cc9d50ea2",
+    //   accounts: ["606ebc0fdf53ba7d03ffaf735064105f9e083119ab91f36e06c9fc67080f8323"],
+    //   gas: 2100000,
+    //   gasPrice: 30000000000,
+    // },
+    // ropsten: {
+    //   url: "https://ropsten.infura.io/v3/nada",
+    //   accounts: ["606ebc0fdf53ba7d03ffaf735064105f9e083119ab91f36e06c9fc67080f8323"],
+    //   gas: 2100000,
+    //   gasPrice: 8000000000,
+    // },
+
+
+    //    hardhat: {
+    //   chainId: 1337
+    // },
+    // mainnet: {
+    //   url: "https://mainnet.infura.io/v3/9e4c4b48907f4beba4ca0f3cc9d50ea2",
+    //   accounts: ["nada"]
+    // },
+    // ropsten: {
+    //   url: "https://ropsten.infura.io/v3/9e4c4b48907f4beba4ca0f3cc9d50ea2",
+    //   accounts: ["nada"]
+    // },
+    // rinkeby: {
+    //   url: "https://rinkeby.infura.io/v3/9e4c4b48907f4beba4ca0f3cc9d50ea2",
+    //   accounts: ["nada"]
+    // }
+
   },
   etherscan: {
     // Your API key for Etherscan
--- src/App.js
+++ src/App.js
@@ -1,64 +1,78 @@
-import logo from './logo.svg';
-import { useState } from 'react';
-import { ethers } from 'ethers';
-import testMint from "./artifacts/contracts/TestShit.sol/TestMint.json"
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import { ethers } from "ethers";
+import testMint from "./artifacts/contracts/TestShit.sol/TestMint.json";
+import "./App.css";
 
-const testMintAddress = "0x88a03f3016Ffb6fF160560325A5F73C9694c1867"
-
-
+const testMintAddress = "0x88a03f3016Ffb6fF160560325A5F73C9694c1867";
 
 function App() {
-  
-
-  const [greeting, setGreetingValue] = useState('')
+  const [greeting, setGreetingValue] = useState("");
 
   async function requestAccount() {
-    await window.ethereum.request({method: 'eth_requestAccounts' })
+    await window.ethereum.request({ method: "eth_requestAccounts" });
   }
 
   async function ClaimNFT() {
-    if (typeof window.ethereum !== 'undefined') {
-      await requestAccount()
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(testMintAddress, testMint.abi, signer)
-      
-      const txn = await contract.setBaseURI('ipfs://bafybeia7epcga7lz3aowuhheeigq4ghr4jyu7bmidn66jhw7wpcglh5joe')
-      await txn.wait()
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        testMintAddress,
+        testMint.abi,
+        signer
+      );
+
+      const txn = await contract.setBaseURI(
+        "ipfs://bafybeia7epcga7lz3aowuhheeigq4ghr4jyu7bmidn66jhw7wpcglh5joe"
+      );
+      await txn.wait();
     }
   }
 
   async function getTokenURI() {
-    if (typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(testMintAddress, testMint.abi, signer)
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        testMintAddress,
+        testMint.abi,
+        signer
+      );
       try {
-        const data = await contract.tokenURI(2)
-        console.log('data: ', data)
+        const data = await contract.tokenURI(2);
+        console.log("data: ", data);
       } catch (err) {
-        console.log("Error: ", err)
+        console.log("Error: ", err);
       }
-    } 
+    }
   }
 
   return (
     <div className="App">
-      <div className='overlay'>
+      <div className="overlay">
+        <div className="imageContainer">
+          <img src="https://media.giphy.com/media/dOR6QEEFEobDI1UBSK/giphy.gif" />
+        </div>
+        <header className="App-header">
+          <h1> WHEREIS22 NFT</h1>
+          <p>
+            WHEREIS22 is an exclusive NFT membership built on the Ethereum
+            blockchain. With WHEREIS22 NFT you'll get utilities across the
+            digital and physical world.
+          </p>
+        </header>
+        <div className="mainContainer">
+          <div className="mintButtons">
+            <button onClick={requestAccount}>Connect</button>
+            <button onClick={getTokenURI}>Mint this bitch</button>
+          </div>
+        </div>
 
-      
-      <header className="App-header">
-        <h1> WHEREIS22 NFT</h1>
-        <p>WHEREIS22 is an exclusive NFT membership built on the Ethereum blockchain. With WHEREIS22 NFT you'll get utilities across the digital and physical world.</p>
-        <button onClick={requestAccount}>Connect</button>
-        <button onClick={getTokenURI}>Mint this bitch</button>
-      </header>
-
-<div className='moving-background'></div>
+        <div className="moving-background"></div>
       </div>
     </div>
-    
   );
 }
 
