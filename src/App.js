7ff8e4a72d4f3ec126d300a7a68a2a79465df6cf
andre2130 <atrobinson30@gmail.com>
2022-05-26 19:38:56 -0400

    removed unused variables

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import { useState } from "react";
 import { ethers } from "ethers";
 import testMint from "./artifacts/contracts/TestShit.sol/TestMint.json";
@@ -7,29 +6,28 @@ import "./App.css";
 const testMintAddress = "0x88a03f3016Ffb6fF160560325A5F73C9694c1867";
 
 function App() {
-  const [greeting, setGreetingValue] = useState("");
 
   async function requestAccount() {
     await window.ethereum.request({ method: "eth_requestAccounts" });
   }
 
-  async function ClaimNFT() {
-    if (typeof window.ethereum !== "undefined") {
-      await requestAccount();
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const contract = new ethers.Contract(
-        testMintAddress,
-        testMint.abi,
-        signer
-      );
+  // async function ClaimNFT() {
+  //   if (typeof window.ethereum !== "undefined") {
+  //     await requestAccount();
+  //     const provider = new ethers.providers.Web3Provider(window.ethereum);
+  //     const signer = provider.getSigner();
+  //     const contract = new ethers.Contract(
+  //       testMintAddress,
+  //       testMint.abi,
+  //       signer
+  //     );
 
-      const txn = await contract.setBaseURI(
-        "ipfs://bafybeia7epcga7lz3aowuhheeigq4ghr4jyu7bmidn66jhw7wpcglh5joe"
-      );
-      await txn.wait();
-    }
-  }
+  //     const txn = await contract.setBaseURI(
+  //       "ipfs://bafybeia7epcga7lz3aowuhheeigq4ghr4jyu7bmidn66jhw7wpcglh5joe"
+  //     );
+  //     await txn.wait();
+  //   }
+  // }
 
   async function getTokenURI() {
     if (typeof window.ethereum !== "undefined") {
