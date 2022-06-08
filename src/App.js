d579f7fe2a059847ab50373a7f22cab4d1f22f53
Jadennftproject <jaden@thenftproject.io>
2022-06-08 05:03:21 -0400

    grammar changes

--- src/App.js
+++ src/App.js
@@ -95,13 +95,20 @@ function App() {
       <h1>WHEREIS22 NFT</h1>
       <h3>Presale Price: 0.12 eth</h3>
       <h4>1. Click connect to connect your wallet</h4>
-      <h4>2. When connected it will say "Congrats! You are allowed to Mint</h4>
+      <h4>2. When connected, you will see "Congrats! You are allowed to Mint"</h4>
       <h4>3. Now click mint and get your NFT</h4>
       <h4>4. After minting your NFT will be in your crypto wallet. </h4>
       <h3 id="address"> </h3>
       <button id="connectButton" onClick={requestAccount}>Connect</button>
       <button onClick={ClaimNFT}>Mint</button>
+
+      
+    
+
       <h3 id="info"> </h3>
+      
+
+
     </div>
   </section>
     </div>
